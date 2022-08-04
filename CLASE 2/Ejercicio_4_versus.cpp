1d84a118f772aec62bfea2330b9ab5ae4fc38b30
Willy David Valentin Ricaldi <67374479+w1llyVR@users.noreply.github.com>
2022-08-04 00:49:31 -0500

    Create Ejercicio_4_versus.cpp

---
+++ CLASE 2/Ejercicio_4_versus.cpp
@@ -0,0 +1,45 @@
+//VERSUS
+
+#include<iostream>
+using namespace std;
+
+void main() {
+	
+	int a1 = 17, r=4;
+	cout << "17 ";
+	for (int i = 1; i <=20; i++)
+	{
+		a1 = a1 + r;
+		cout << a1 << " ";
+	}
+
+	system("pause");
+
+}
+
+/*
+#include<iostream>
+using namespace std;
+
+void main() {
+	
+	int i = 1;
+	int a1 = 17, r = 4;
+
+	cout << "17 ";
+
+	while (i<=20)
+	{
+		a1 = a1 + r;
+		cout << a1 << " ";
+		i++;
+	}
+
+
+
+
+	system("pause");
+
+}
+
+*/
