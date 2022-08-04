3f19877ae2b3629ae7de875d3f2250c64f3d03c3
Willy David Valentin Ricaldi <67374479+w1llyVR@users.noreply.github.com>
2022-08-04 00:47:00 -0500

    Create Ejercicio_2.cpp

---
+++ CLASE 2/Ejercicio_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void main() {
+	//i =123;
+
+
+	for (int i = 32; i <= 164; i++)
+	{
+		if (i <= 122 && i >= 97) continue;
+		cout << char(i) << endl;
+	}
+	
+														
+	system("pause");
+
+}
